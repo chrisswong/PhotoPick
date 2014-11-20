039978a0fd8ab442b0fd79a28dcd776763afc8cb
Chris Wong <chris.wong@gtomato.com>
2014-11-21 00:17:13 +0800

    init project.  ADDED ImagePickerController to pick images

---
+++ PhotoLike/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  PhotoLike
+//
+//  Created by chris on 21/11/14.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#ifndef PhotoLike_Common_h
+#define PhotoLike_Common_h
+
+
+#endif
--- PhotoLike/ViewController.m
+++ PhotoLike/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () 
 
 @end
 
@@ -22,6 +22,38 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)givePhotoAcessButtonDidTapped:(UIButton *)sender {
+
+    UIImagePickerController *pickerController = [[UIImagePickerController alloc] init];
+    pickerController.delegate = self;
+    [self presentViewController:pickerController animated:YES completion:NULL];
+}
+
+
+#pragma mark - UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *selectedImage = info[UIImagePickerControllerOriginalImage];
+    
+    NSData *imageData = UIImagePNGRepresentation(selectedImage);
+    
+    NSArray* cachePathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString* cachePath = [cachePathArray lastObject];
+    
+    NSInteger randomFileNo = arc4random_uniform(100);
+    
+    NSString *fileName = [NSString stringWithFormat:@"%d.png", randomFileNo];
+    
+    NSString *filePath = [cachePath stringByAppendingPathComponent:fileName];
+    
+    [imageData writeToFile:filePath atomically:YES];
+    
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
 }
 
 @end
