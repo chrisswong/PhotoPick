6355135ad5b5936cf39f5f16d43a4b89c2e33bb1
Chris Wong <chris.wong@gtomato.com>
2014-11-22 11:09:04 +0800

    ADDED today extension target

---
+++ PhotoToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  PhotoToday
+//
+//  Created by chris on 21/11/14.
+//  Copyright (c) 2014 chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
